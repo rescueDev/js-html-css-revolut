bd781304b6ffc630469afdc63c8640ed1d9d64ea
Salvatore Borgia <sa.borgia3@gmail.com>
2020-11-05 10:32:25 +0100

    mouseleave autoclosing dropdown

--- js/script.js
+++ js/script.js
@@ -7,10 +7,16 @@ $(document).ready(function () {
   linkNav.click(function () {
     //rimuovo classe a tutti
     $(".menu-shown").removeClass("menu-shown");
-    //aggiungo a tutti
+    //aggiungo a tutti i dropdown-menu la classe menu-shown
     $(this).find(".dropdown-menu").addClass("menu-shown");
     //faccio sparire tutti gli eventuali dropdown-menu aperti
     $(".dropdown-menu").not(".menu-shown").fadeOut();
+    //faccio il fadetoggle dei dropdown-menu
     $(this).find(".dropdown-menu").fadeToggle();
+  });
+
+  //evento mouseleave
+  $(".dropdown-menu").mouseleave(function () {
+    $(".dropdown-menu").fadeOut();
   });
 });
